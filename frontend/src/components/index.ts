324d45ec0767e227fab9d1ab2dbaf194c8a6f451
Chase Peterson <cep.piano@gmail.com>
2025-01-08 12:02:26 -0800

    Created basic skeleton for navbar and marketplace, disabled linting

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,3 @@
 // the no-relative-import-paths eslint plugin doesn't check exports
 // IMO this is fine for an index file
-export { Home } from "./Home";
-export { Marketplace } from "./Marketplace";
+export { Navbar } from "./Navbar";
