496c38dd0b5d18644571d28ee0671bc41c8a7532
nselvakumar25 <nselvakumar@ucsd.edu>
2025-01-10 18:16:07 -0800

    change color of product card and add link to product component

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -6,15 +6,17 @@ interface Props {
 
 function Product({ productImage, productName, productPrice }: Props) {
   return (
-    <div className="max-w-[16rem] w-[16rem] bg-white shadow-xl">
-      <div className="max-h-[16rem] h-[16rem] overflow-hidden">
-        <img className="w-full h-full object-cover" src={productImage} alt="" />
+    <a href="/">
+      <div className="max-w-[16rem] w-[16rem] bg-[#F8F8F8] shadow-xl">
+        <div className="max-h-[16rem] h-[16rem] overflow-hidden">
+          <img className="w-full h-full object-cover" src={productImage} alt="" />
+        </div>
+        <div className="p-2">
+          <p className="font-semibold">{productName}</p>
+          <p className="font-extralight">${productPrice.toFixed(2)}</p>
+        </div>
       </div>
-      <div className="p-2">
-        <p className="font-semibold">{productName}</p>
-        <p className="font-extralight">${productPrice.toFixed(2)}</p>
-      </div>
-    </div>
+    </a>
   );
 }
 
