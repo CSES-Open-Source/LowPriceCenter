1304dbc10f98a3eadff35ede210c690d5e528f2b
nselvakumar25 <nselvakumar@ucsd.edu>
2025-01-18 18:31:48 -0800

    show confirmation for adding product

--- frontend/src/pages/AddProduct.tsx
+++ frontend/src/pages/AddProduct.tsx
@@ -1,25 +1,32 @@
-import { FormEvent, useRef, useContext } from "react";
+import { FormEvent, useRef, useContext, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { Navbar } from "src/components";
 import { post } from "src/api/requests";
 import { FirebaseContext } from "src/utils/FirebaseProvider";
+import { CiCircleCheck } from "react-icons/ci";
 
 export function AddProduct() {
   const productName = useRef<HTMLInputElement>(null);
   const productPrice = useRef<HTMLInputElement>(null);
   const productDescription = useRef<HTMLTextAreaElement>(null);
   const productImages = useRef<HTMLInputElement>(null);
+  const [productAdded, setProductAdded] = useState<boolean>(false);
   const { user } = useContext(FirebaseContext);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    if (productName.current && productPrice.current && productDescription.current && user) {
-      return await post("/api/products", {
-        name: productName.current.value,
-        price: productPrice.current.value,
-        description: productDescription.current.value,
-        userEmail: user.email,
-      });
+    try {
+      if (productName.current && productPrice.current && productDescription.current && user) {
+        const res = await post("/api/products", {
+          name: productName.current.value,
+          price: productPrice.current.value,
+          description: productDescription.current.value,
+          userEmail: user.email,
+        });
+        if (res.ok) setProductAdded(true);
+      }
+    } catch (err) {
+      //handle error
     }
   };
 
@@ -55,6 +62,8 @@ export function AddProduct() {
           <input
             id="productPrice"
             type="number"
+            min={0}
+            step={0.01}
             ref={productPrice}
             className="border border-gray-300 text-black text-sm rounded-md w-full p-2.5 y-600"
             placeholder="$0.00"
@@ -97,16 +106,35 @@ export function AddProduct() {
             Submit
           </button>
         </div>
-        {/* {productImages.current && productImages.current.files && (
-          <div>
-            <p>image preview</p>
-            <img
-              src={URL.createObjectURL(productImages.current.files[0])}
-              className="w-32 h-32 object-cover border rounded"
-            />
-          </div>
-        )} */}
       </form>
+      <dialog open={productAdded}>
+        <div className="backdrop-blur-[2px] flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center mt-20 w-full md:inset-0 h-[calc(100%-1rem)]">
+          <div className="relative p-4 w-full max-w-2xl max-h-full">
+            <div className="relative bg-blue-50 rounded-lg shadow-2xl">
+              <div className="flex flex-col items-center gap-3 p-4 md:p-5 border-b">
+                <CiCircleCheck className="text-green-600 w-10 h-10" />
+                <h3 className="text-xl font-semibold text-gray-900">
+                  Your product is now on the marketplace!
+                </h3>
+                <div className="flex flex-row gap-10 mt-8">
+                  <button
+                    className="text-[#00629B] font-semibold py-2 px-4 hover:text-blue-900"
+                    onClick={() => (window.location.href = "/add-product")}
+                  >
+                    Add Another Product
+                  </button>
+                  <button
+                    className="text-[#00629B] font-semibold py-2 px-4  hover:text-blue-900"
+                    onClick={() => (window.location.href = "/marketplace")}
+                  >
+                    View Marketplace
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </dialog>
     </>
   );
 }
