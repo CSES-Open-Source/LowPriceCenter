ade4d4b9d0c31fbf17998f9f17d61cbff0cfca33
nselvakumar25 <nselvakumar@ucsd.edu>
2025-01-19 18:25:20 -0800

    fetch products for marketplace grid

--- frontend/src/pages/Marketplace.tsx
+++ frontend/src/pages/Marketplace.tsx
@@ -1,27 +1,33 @@
+import { Key, useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { get } from "src/api/requests";
 import Product from "src/components/Product";
 
-const placeholderData: [string, number, string][] = [
-  ["24oz Water Bottle", 15.0, "productImages/water-bottle.png"],
-  ["Shower Caddy", 16.0, "productImages/shower-caddy.jpeg"],
-  ["Mattress", 169.75, "productImages/mattress.png"],
-  ["Vintage Lamp", 30.0, "productImages/vintage-lamp.jpeg"],
-  ["Vintage Lamp", 30.0, "productImages/vintage-lamp.jpeg"],
-  ["Mattress", 169.75, "productImages/mattress.png"],
-  ["24oz Water Bottle", 15.0, "productImages/water-bottle.png"],
-  ["Shower Caddy", 16.0, "productImages/shower-caddy.jpeg"],
-  ["24oz Water Bottle", 15.0, "productImages/water-bottle.png"],
-  ["Shower Caddy", 16.0, "productImages/shower-caddy.jpeg"],
-];
-
 export function Marketplace() {
+  const [products, setProducts] = useState<[]>();
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    try {
+      const fetchProducts = async () => {
+        await get("/api/products/").then(async (response) => {
+          const productData = await response.json();
+          setProducts(productData);
+        });
+      };
+      fetchProducts();
+    } catch (err) {
+      setError(true);
+    }
+  }, []);
+
   return (
     <>
       <Helmet>
         <title>Low-Price Center Marketplace</title>
       </Helmet>
       <main className="w-full flex justify-center items-center mt-12 mb-20">
-        <div className="max-w-[80%]">
+        <div className="max-w-[80%] w-full">
           <div id="grid-header" className="flex justify-between flex-wrap mb-2 px-3">
             <p className="text-3xl font-mono font-medium">Marketplace</p>
             <button
@@ -31,19 +37,29 @@ export function Marketplace() {
               Add Product
             </button>
           </div>
+          {/* Error message if products cannot be displayed */}
+          {error && (
+            <p className="max-w-[80%] w-full px-3 text-red-800">
+              Unable to display products. Try again later.
+            </p>
+          )}
+          {/* Grid of products */}
           <div
             id="grid"
             className="grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xxl:grid-cols-4"
           >
-            {placeholderData.map((product, index) => (
-              <div key={index} className="px-3 py-3">
-                <Product
-                  productName={product[0]}
-                  productPrice={product[1]}
-                  productImage={product[2]}
-                />
-              </div>
-            ))}
+            {products &&
+              products.map(
+                (product: { name: string; price: number }, index: Key | null | undefined) => (
+                  <div key={index} className="px-3 py-3">
+                    <Product
+                      productName={product.name}
+                      productPrice={product.price}
+                      productImage={"productImages/product-placeholder.webp"}
+                    />
+                  </div>
+                ),
+              )}
           </div>
         </div>
       </main>
