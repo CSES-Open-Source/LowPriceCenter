4af99e34c73cd8dac3f75d22aeb1cd885db506f1
nicholas-ngyn <nicholas.ngyn@gmail.com>
2025-01-22 14:19:08 -0800

    updated individual-product-page

--- frontend/src/pages/Individual-product-page.tsx
+++ frontend/src/pages/Individual-product-page.tsx
@@ -1,17 +1,14 @@
 import { Helmet } from "react-helmet-async";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
-
 
 const placeholderData = [
-    {
-      productName: "Mattress",
-      productPrice: 500,
-      productImage: "productImages/mattress.png",
-      sellerEmail: "ucsd@example.com",
-      productInfo: "A comfortable mattress with memory foam for a good night's sleep.",
-    },
+  {
+    productName: "Mattress",
+    productPrice: 500,
+    productImage: "productImages/mattress.png",
+    sellerEmail: "ucsd@example.com",
+    productInfo: "A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.A comfortable mattress with memory foam for a good night's sleep.",
+  },
 ];
 
 export function IndividualProductPage() {
@@ -23,55 +20,52 @@ export function IndividualProductPage() {
       <Helmet>
         <title>{product.productName} - Low-Price Center</title>
       </Helmet>
-      <main className="w-[80%] max-w-screen-2xl mx-auto mt-12">
+      <main className="w-[80%] max-w-screen-2xl mx-auto m-12">
         <button
           className="text-lg mb-4 font-inter hover:underline"
           onClick={() => navigate("/marketplace")}
         >
-          <FontAwesomeIcon className="pr-2" icon={faArrowLeft} aria-label="Left-Arrow"/>
-          Return to Marketplace
+          &larr; Return to Marketplace
         </button>
         <div className="flex flex-wrap flex-col md:flex-row mb-6 gap-10">
           {/* Image Section */}
-            <div className="bg-ucsd-blue max-w-full w-full h-[606px] flex-1 flex items-center justify-center md:h-auto">
-              <div className="w-full max-w-lg aspect-square bg-ucsd-blue">
-                <img
-                  src={product.productImage}
-                  alt="Product"
-                  className="w-full h-full object-contain"
-                />
-              </div>
+          <section className="bg-ucsd-blue max-w-full w-full max-h-[606px] flex-1 flex items-center justify-center md:h-auto">
+            <div className="w-full max-w-lg aspect-square bg-ucsd-blue">
+              <img
+                src={product.productImage}
+                alt="Product"
+                className="w-full h-full object-contain"
+              />
             </div>
+          </section>
 
           {/* Info Section */}
-          <div className="max-w-full h-[606px] flex-1 flex flex-col">
-            <div className="pt-4">
-              <h1 className="font-jetbrains text-black font-bold text-4xl pb-1">
-                {product.productName}
-              </h1>
-            </div>
+          <section className="max-w-full flex-1 flex flex-col">
+            <h1 className="pt-2 font-jetbrains text-black font-bold text-4xl">
+              {product.productName}
+            </h1>
 
-            <hr className="my-6 w-full mx-auto border-t-4 border-gray-200" />
-
-            <div className="flex flex-col flex-grow">
-              <h2 className="font-jetbrains text-black-gray font-bold text-2xl pb-6">
-                USD ${product.productPrice.toFixed(2)}
-              </h2>
-              <div className="bg-[#F5F0E6] p-5 flex-grow overflow-y-auto max-h-[200px] md:max-h-[250px] lg:max-h-[300px]">
-                <p className="font-inter text-black">
-                    {product.productInfo}
+            <hr className="my-6 w-full mx-auto h-0 border-[1px] border-solid border-gray-300" />
+            
+            <h2 className="font-inter text-black-gray text-xl font-normal pb-6">
+              USD ${product.productPrice.toFixed(2)}
+            </h2>
+            {product.productInfo && (
+              <div className="bg-[#F5F0E6] p-5 mb-6">
+                <p className="font-inter text-black text-xl font-normal">
+                  {product.productInfo}
                 </p>
               </div>
-              <div className="mt-auto">
-                <p className="font-inter text-black text-lg">
-                  Interested? Contact them here:
-                </p>
-                <p className="font-inter text-black text-lg font-bold">
-                  {product.sellerEmail}
-                </p>
-              </div>
+            )}
+            <div className="mt-0">
+              <p className="font-inter text-black text-xl font-light">
+                Interested? Contact them here:
+              </p>
+              <p className="font-inter text-black text-xl font-medium">
+                {product.sellerEmail}
+              </p>
             </div>
-          </div>
+          </section>
         </div>
       </main>
     </>
