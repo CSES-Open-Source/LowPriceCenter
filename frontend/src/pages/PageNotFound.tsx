3e712e39203e578cc4d16f4b813e2f4e93c10edb
n1sh1thaS <nishi2919@gmail.com>
2025-03-05 16:20:51 -0800

    make image more responsive

--- frontend/src/pages/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound.tsx
@@ -1,7 +1,7 @@
 export function PageNotFound() {
   return (
-    <div className="flex flex-row items-center justify-center mt-14 gap-3 md:gap-20 flex-wrap">
-      <img src="/404.png" className="w-1/3 h-auto" alt="404" />
+    <div className="flex flex-row items-center justify-center h-[76vh] gap-3 md:gap-20">
+      <img src="/404.png" className="w-80 md:w-1/3 h-auto" alt="404" />
     </div>
   );
 }
